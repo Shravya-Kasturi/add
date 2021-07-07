257a33e7dbdf0cca8d8c35b39f208be50ebd3729
shravya <shravya.kasturi@leanix.net>
2021-07-08 02:36:13 +0530

    feat(core): add msg

--- dist/index.js
+++
@@ -1,2 +0,0 @@
-"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),exports.add=function(e,s){return console.log("this is new npm package working nicely. Please publish!!!"),e+s};
-//# sourceMappingURL=index.js.map
