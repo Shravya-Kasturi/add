17a663c2cc422a696ace240e101522484e4a6fd4
shravya <shravya.kasturi@leanix.net>
2021-07-08 02:30:07 +0530

    feat(core):add msg

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 function add(x:number, y: number) : number{
-  console.log('this is new npm package working nicely. Please publish!!!');
+  console.log('this is new npm package working nicely. Please publish!');
   return x + y;
 }
 
