3eb5f8e48f1d33a04526f89b6af523adfc7d2c0c
shravya <shravya.kasturi@leanix.net>
2021-07-08 20:24:41 +0530

    feat(core): add minus function

---
+++ src/minus.ts
@@ -0,0 +1,8 @@
+function minus(x: number, y: number): number {
+    console.log('this is new npm package working nicely. Please publish!!!');
+  
+    return x - y;
+  }
+  
+  export { minus };
+  
