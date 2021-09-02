1267fdc4e1458ea177791c1314ab491852cfc6d0
shravya <shravya.kasturi@leanix.net>
2021-09-02 19:25:39 +0530

    chore(core): formatting

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,5 @@ import { add } from './main/add';
 import { multiply } from './main/multiply';
 import { divide } from './main/divide';
 import { modulus } from './main/modulus';
-import { square }  from './main/square'
+import { square } from './main/square';
 export { add, minus, multiply, divide, modulus, square };
--- src/main/square.ts
+++ src/main/square.ts
@@ -1,8 +1,7 @@
 function square(x: number): number {
-    console.log('this is new npm package working nicely. Please publish!!!');
-  
-    return x * x;
-  }
-  
-  export { square };
-  
+  console.log('this is new npm package working nicely. Please publish!!!');
+
+  return x * x;
+}
+
+export { square };
