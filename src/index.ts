2207f237ae55f43927952cecb9890e4c45c382fb
Shravya-Kasturi <chittishravi123@gmail.com>
2021-09-02 12:28:02 +0530

    feat(modulus): added new modulus feature

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ import { minus } from './main/minus';
 import { add } from './main/add';
 import { multiply } from './main/multiply';
 import { divide } from './main/divide';
+import { modulus } from './main/modulus';
 export { add, minus, multiply, divide };
